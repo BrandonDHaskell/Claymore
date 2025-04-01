1e87c629e3f15fd1c23db23a13a6a325b707a2e3
Brandon Haskell <BrandonDHaskell@outlook.com>
2025-04-01 07:30:59 -0700

    add JSON presets and options to cmake

---
+++ cmake/options.cmake
@@ -0,0 +1,4 @@
+# Command line options for Claymore
+option(CLAY_INCLUDE_ALL_EXAMPLES "Build example apps" OFF)
+option(CLAYMORE_ENABLE_EXTERNALS "Use external submodules" OFF)
+option(CLAYMORE_DEV_MODE "Enable dev utilities and logging" ON)
